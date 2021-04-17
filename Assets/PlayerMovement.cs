8b8debe5b927b0fa306b96a3b90a535ae21ba77f
xrayman021 <chaseghorton99@gmail.com>
2021-04-17 15:41:41 -0500

    Messed with PlayerMovement script

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //public CharacterController controller;
+    public CharacterController controller;
 
     public float speed = 12f;
     private float originalSpeed;
