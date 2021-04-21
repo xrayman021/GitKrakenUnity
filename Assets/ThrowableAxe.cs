524e9d70e431a3502fe79d34162c883bdd130cf8
xrayman021 <chaseghorton99@gmail.com>
2021-04-21 14:19:24 -0500

    Began Axe Throw code

---
+++ Assets/ThrowableAxe.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowableAxe : MonoBehaviour
+{
+    public Rigidbody axe;
+    public float throwForce = 50;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonUp("Fire1"))
+        {
+            ThrowAxe();
+        }
+    }
+
+    //Throw axe
+    void ThrowAxe()
+    {
+        axe.isKinematic = false;
+        axe.AddForce(Camera.main.transform.TransformDirection(Vector3.forward) * throwForce);
+    }
+
+    //Return Axe
+    //Reset Axe
+
+}
