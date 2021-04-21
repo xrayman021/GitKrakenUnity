b0a3708debccdcbd952d6e954329c6e94e736ef2
xrayman021 <chaseghorton99@gmail.com>
2021-04-21 16:10:47 -0500

    Finished Return and ResetAxe code

--- Assets/ThrowableAxe.cs
+++ Assets/ThrowableAxe.cs
@@ -6,6 +6,10 @@ public class ThrowableAxe : MonoBehaviour
 {
     public Rigidbody axe;
     public float throwForce = 50;
+    public Transform target, curve_point;
+    private Vector3 old_pos;
+    private bool isReturning = false;
+    private float time = 0.0f;
 
     // Update is called once per frame
     void Update()
@@ -14,11 +18,28 @@ public class ThrowableAxe : MonoBehaviour
         {
             ThrowAxe();
         }
+        if (Input.GetButtonUp("Fire2"))
+        {
+            ReturnAxe();
+        }
+        if(isReturning)
+        {
+            if(time < 1.0f)
+            {
+                axe.position = getBQCPoint(time, old_pos, curve_point.position, target.position);
+                axe.rotation = Quaternion.Slerp(axe.transform.rotation, target.rotation, 50 * Time.deltaTime);
+            }
+            else
+            {
+                ResetAxe();
+            }
+        }
     }
 
     //Throw axe
     void ThrowAxe()
     {
+        isReturning = false;
         axe.transform.parent = null;
         axe.isKinematic = false;
         axe.AddForce(Camera.main.transform.TransformDirection(Vector3.forward) * throwForce, ForceMode.Impulse);
@@ -26,6 +47,30 @@ public class ThrowableAxe : MonoBehaviour
     }
 
     //Return Axe
+    void ReturnAxe()
+    {
+        time = 0.0f;
+        old_pos = axe.position;
+        isReturning = true;
+        axe.velocity = Vector3.zero;
+        axe.isKinematic = true;
+    }
     //Reset Axe
+    void ResetAxe()
+    {
+        isReturning = false;
+        axe.transform.parent = transform;
+        axe.position = target.position;
+        axe.rotation = target.rotation;
+    }
+    Vector3 getBQCPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        Vector3 p = (uu * p0) + (2 * u * t * p1) + (tt * p2);
+        return p;
+    }
+
 
 }
